4c4b78b75c6c325e57765b681c79a1a29cfb352c
jxj <2045927977@qq.com>
2025-05-07 21:00:28 +0800

    transforms使用

--- scripts/test_3.py
+++ scripts/test_3.py
@@ -3,35 +3,60 @@
 """
 
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
-# TensorBoard # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+# transforms # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # ##
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 
-# TensorBoard ##########################################################################################################
-# SummaryWriter: 用于创建日志文件，可以在TensorBoard中可视化
-# add_scalar: 将单个数值按时间步记录到日志中，语法为add_scalar(图表标签, 数值, 时间步)，其中数值即y轴，时间步即x轴
-# add_image: 将图像数据按时间步记录到日志中，语法为add_image(图表标签, 图像, global_step=时间步, dataformats=根据图像类型确定)
-# TensorBoard可视化：
-#   一般用法：终端输入tensorboard --logdir=事件文件所在文件夹名，然后在浏览器中访问端口即可
-#   指定端口：终端输入tensorboard --logdir=事件文件所在文件夹名 --port=端口号(如6007)
+# transforms ###########################################################################################################
+# transforms用于图像的预处理和数据增强，如转换为张量，归一化，尺寸调整，图像裁剪，调整亮度、对比度、饱和度等，图像翻转等
+# ToTensor():
+# Normalize():
+# Resize():
+# Compose(): 用于将多个图像变换操作组合在一起按顺序执行
+# RandomCrop(): 对图像进行随机裁剪
 ########################################################################################################################
 
-# 写一个TensorBoard的例子 ------------------------------------------------------------------------------------------------
+# 写一个transforms的例子 -------------------------------------------------------------------------------------------------
 from torch.utils.tensorboard import SummaryWriter
-import math
 from PIL import Image
-import numpy as np
+from torchvision import transforms
 
-writer = SummaryWriter("../runs/test_2")    # 指定结果存放位置
-x = range(0, 10)                            # 生成时间步(假设这就是epoch)
+writer = SummaryWriter("../runs/test_3")
+img_pil = Image.open("../data/hymenoptera_data/train/ants/0013035.jpg")
 
-# 将数值记录到日志中
-for i in x:
-    writer.add_scalar('y=2x', math.cos(0.1*i) * 2, i)
+# ToTensor
+tensor_trans = transforms.ToTensor()    # 返回一个类对象
+img_tensor = tensor_trans(img_pil)      # 本质上为调用__call__函数
 
-# 将图像记录到日志中
-img_pil = Image.open("../data/hymenoptera_data/train/ants/5650366_e22b7e1065.jpg")  # 读取图像，该方式读后存储的类型为pil
-img_np = np.array(img_pil)                                                          # 将图像类型转换为被支持的格式
-for i in range(5):
-    writer.add_image("image", img_np, global_step=i, dataformats="HWC")
+# Normalize
+norm_trans = transforms.Normalize([0.5, 0.5, 0.5], [0.5, 0.5, 0.5])
+img_norm = norm_trans(img_tensor)
+
+# Resize
+resize_trans = transforms.Resize((512, 512))
+img_resize = resize_trans(img_pil)
+img_resize = tensor_trans(img_resize)
+
+# Compose
+com_trans = transforms.Compose([
+    transforms.Resize((224, 224)),  # 将图像调整为 224x224 大小
+    transforms.ToTensor(),  # 将图像转换为张量
+    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])  # 对图像进行归一化处理
+])
+img_com = com_trans(img_pil)
+
+# RandomCrop
+random_trans = transforms.RandomCrop((512, 512))
+# img_random = random_trans(img_pil)
+# img_random = tensor_trans(img_random)
+
+writer.add_image("tensor_img", img_tensor, 0, dataformats="CHW")
+writer.add_image("norm_img", img_norm, 0, dataformats="CHW")
+writer.add_image("resize_img", img_resize, 0, dataformats="CHW")
+writer.add_image("com_img", img_com, 0, dataformats="CHW")
+for i in range(10):
+    img_random = random_trans(img_pil)
+    img_random = tensor_trans(img_random)
+    writer.add_image("random_img", img_random, i, dataformats="CHW")
 
 writer.close()
+#-----------------------------------------------------------------------------------------------------------------------
